f67e6d9bad37dd890fba2b8cad5e2971da5af843
Nicolas Audinet <nic.audinet27@gmail.com>
2022-11-27 11:06:51 +0100

    Refactor to use the Spot class

--- self-avoiding-walk.py
+++ self-avoiding-walk.py
@@ -1,38 +1,54 @@
 import pygame
 import random
-import numpy as np
-import sys
 import copy
 
 FPS = 20
 
-STRIDE = 20
-ROWS = 50
-COLS = 50
+STRIDE = 50
+ROWS = 10
+COLS = 10
 WIDTH = COLS * STRIDE
 HEIGHT = ROWS * STRIDE
 
 COLOR = (150,150,150)
-RADIUS = 5
+RADIUS = 15
 
 ALL_OPTIONS = [(-1,0),(1,0),(0,-1),(0,1)]
 
+class Spot:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+        self.pos = (i * STRIDE + STRIDE / 2, j * STRIDE + STRIDE / 2)
+        self.visited = False
+
+    def is_valid(self, i, j, grid):
+        within_bounds = i >= 0 and i < COLS and j >= 0 and j < ROWS
+        return within_bounds and not grid[i][j].visited
+
+    def step(self, grid):
+        options = []
+        for option in ALL_OPTIONS:
+            i = self.i + option[0]
+            j = self.j + option[1]
+            if self.is_valid(i, j, grid):
+                options.append(option)
+        if options == []:
+            return None
+        else:
+            option = random.choice(options)
+            i = self.i + option[0]
+            j = self.j + option[1]
+            return grid[i][j]
+
 def init_grid():
-    return np.ones((ROWS,COLS), dtype=np.bool)
-
-def update_grid(grid):
+    grid = []
+    for i in range(COLS):
+        row = []
+        for j in range(ROWS):
+            row.append(Spot(i,j))
+        grid.append(row)
     return grid
-
-def t(grid):
-    x = grid[0] * STRIDE
-    y = grid[1] * STRIDE
-    return (x,y)
-
-def is_valid(curr, grid, option):
-    x = int(curr[0] + option[0])
-    y = int(curr[1] + option[1])
-    within_bounds = x > 0 and x < COLS and y > 0 and y < ROWS
-    return within_bounds and not grid[x,y]
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -42,29 +58,24 @@ pygame.display.set_caption("Self-Avoiding Walk")
 animate = True
 exit = False
 
-curr = (ROWS / 2, COLS / 2)
-pygame.draw.circle(screen, COLOR, t(curr), RADIUS)
-grid = np.zeros((ROWS,COLS))
+grid = init_grid()
+curr = grid[0][0]
+pygame.draw.circle(screen, COLOR, curr.pos, RADIUS)
 
 while not exit:
 
     clock.tick(FPS)
 
     if animate:
-        options = []
-        for option in ALL_OPTIONS:
-            if is_valid(curr, grid, option):
-                options.append(option)
-        if options == []:
+        prev = copy.deepcopy(curr)
+        curr = curr.step(grid)
+        if not curr:
             print("Got stuck!")
             animate = False
         else:
-            option = random.choice(options)
-            prev = copy.deepcopy(curr)
-            curr = (int(curr[0] + option[0]), int(curr[1] + option[1]))
-            pygame.draw.circle(screen, COLOR, t(curr), RADIUS)
-            pygame.draw.line(screen, COLOR, t(prev), t(curr))
-            grid[curr] = True
+            pygame.draw.circle(screen, COLOR, curr.pos, RADIUS)
+            pygame.draw.line(screen, COLOR, prev.pos, curr.pos, width=5)
+            curr.visited = True
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
